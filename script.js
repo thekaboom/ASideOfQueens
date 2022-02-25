c0bfea2601a30964f0aadb8f78d117386896bc79
thekaboom <65932398+thekaboom@users.noreply.github.com>
2022-02-25 17:04:47 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -35,3 +35,56 @@ function draw() {
   }
 }
 loop();
+
+const editable = document.querySelector('.changing-text');
+
+const words = [
+  'BRUNCH',
+  'MIMOSAS',
+  'EVENTS',
+  'PARTIES',
+  'DINNER',
+  'CONCERTS',
+  'MUSICALS',
+  'LUNCH',
+];
+const animationTimeInms = 15;
+const delayInms = 1000;
+
+function close(index) {
+  editable.textContent = words[index % words.length];
+  const interval = setInterval(function() {
+    const text = editable.textContent;
+    if (!text) {
+      clearInterval(interval);
+      setTimeout(function() {
+        open(++index);
+      }, 100);
+    }
+    editable.textContent = text.slice(0, -1);
+  }, animationTimeInms);
+}
+
+function open(index) {
+  const initialText = words[index % words.length];
+  let i = 1;
+  editable.textContent = '';
+  const interval = setInterval(function() {
+    const text = editable.textContent;
+    console.log(text.length === initialText.length);
+    if (text.length === initialText.length) {
+      clearInterval(interval);
+      setTimeout(function() {
+        close(index);
+      }, delayInms);
+    }
+    editable.textContent = initialText.slice(0, i++);
+  }, animationTimeInms);
+}
+
+function writeWords() {
+  const i = 0;
+  open(i);
+}
+
+writeWords();

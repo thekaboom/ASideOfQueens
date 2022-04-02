d22e9e0cfa294e4905c4edadc2022585723b6c0e
thekaboom <65932398+thekaboom@users.noreply.github.com>
2022-04-02 03:33:07 -0500

    UPDATE

---
+++ js/pop.js
@@ -0,0 +1,12 @@
+
+
+$ = function(id) {
+  return document.getElementById(id);
+}
+
+var show = function(id) {
+	$(id).style.display ='block';
+}
+var hide = function(id) {
+	$(id).style.display ='none';
+}
